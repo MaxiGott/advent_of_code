daf37f2cd97f6926a9d595a1083f486b9b36af26
UWF4Y67 <maximilian.gottfried@cariad.technology>
2022-12-01 11:50:07 +0100

    Cleanup code

--- src/challenges/day_1.rs
+++ src/challenges/day_1.rs
@@ -9,12 +9,12 @@ fn read_file_string(filepath: &str) -> Result<String, Box<dyn std::error::Error>
 }
 
 struct Elf {
-    elf_id: i32,
+    elf_id: usize,
     calories: i32
 }
 
 impl Elf {
-    pub fn new(id: i32, calories: i32) -> Self {
+    pub fn new(id: usize, calories: i32) -> Self {
         Elf {
             elf_id: id,
             calories
@@ -22,39 +22,38 @@ impl Elf {
     }
 }
 
-
 pub fn show_results_day_1(){
+    // read file
     let input_res = read_file_string("data/input_1.txt");
     let input = match input_res {
         Ok(value) => value,
         Err(err) => panic!("{}", err)
     };
-
-    let mut elf_counter = 0;
+    let stripped_input = input.trim(); // remove empty lines at beginning and end
+    
     let mut elfs_matching = Vec::new();
 
-    for elf in input.split("\n\n") {
-        let mut cals_as_int = Vec::new();
+    // iterate over all elf blocks
+    for (idx, elf) in stripped_input.split("\n\n").enumerate() {
+        let mut elfs_sum= 0;
         for cal in elf.split("\n"){
-            if cal != "" {
-                let cal_as_int = cal.parse::<i32>().unwrap();
-                cals_as_int.push(cal_as_int);
-            }
+            let cal_as_int = cal.parse::<i32>().unwrap();  // string to integer
+            elfs_sum += cal_as_int;
         }
-        let sum: i32 = cals_as_int.iter().sum();
-        let elf = Elf::new(elf_counter, sum);
+        // create new elf with id and sum of calories
+        let elf = Elf::new(idx, elfs_sum);
         elfs_matching.push(elf);
-
-        elf_counter += 1;
-
     }
+
+    // sort list by calories (descending)
     elfs_matching.sort_by(|a, b| b.calories.cmp(&a.calories));
 
+    // get elf with highest calories
     let fattest_elf = elfs_matching.first().unwrap();
     println!("Most calories: {}", fattest_elf.calories);
 
+    // get the top three calories
     let best_three = elfs_matching.iter().take(3);
-
     let mut total_sum = 0;
     for elf in best_three{
         total_sum += elf.calories;
